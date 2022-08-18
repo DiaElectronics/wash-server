21e1090260ddf0dc92031a6bc8e83a6b7134e205
Roman Fedyashov <roman.fedyashov@gmail.com>
2022-08-19 02:25:09 +0700

    added endpoint for hal

--- dockerfiles/storage/entrypoint.sh
+++ dockerfiles/storage/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-/storage --kasse.endpoint https://10.5.0.11:8443 --db.host 10.5.0.20 --db.port 5432 --db.user wash --db.name wash --db.pass adm-pass-test --goose.dir /migration
+/storage --hal.endpoint 10.5.0.12:8099 --kasse.endpoint https://10.5.0.11:8443 --db.host 10.5.0.20 --db.port 5432 --db.user wash --db.name wash --db.pass adm-pass-test --goose.dir /migration
